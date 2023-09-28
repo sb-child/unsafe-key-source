bc7729b3330daf9704f821b58d131c5d666531f9
sbchild <sbchild0@gmail.com>
2023-09-29 03:19:21 +0800

    fix scripts

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # read __memory.x.sum file
 MEMORY_FILE=$(cat __memory.x.sum)
 # calculate __memory.x.sum
-sha256sum ./memory.x > __memory.x.sum
+sha256sum ./memory.x >__memory.x.sum
 # compare __memory.x.sum and MEMORY_FILE
 if [ "$MEMORY_FILE" != "$(cat __memory.x.sum)" ]; then
     echo "memory.x file changed. Clean caches..."
@@ -11,11 +11,14 @@ if [ "$MEMORY_FILE" != "$(cat __memory.x.sum)" ]; then
     echo "Done."
 fi
 
-# cargo update
-cargo build --release
-mv ./target/thumbv7m-none-eabi/release/unsafe-key ./exec
-arm-none-eabi-objcopy ./exec -O binary ./exec.bin
+cargo update
+
+# cargo install cargo-binutils
+# rustup component add llvm-tools-preview
+cargo objcopy --bin unsafe-key --release --target thumbv7m-none-eabi -- -O binary ./exec.bin
+
 du -h ./exec.bin
-stm32flash -w ./exec.bin -v -g 0x00 /dev/ttyUSB0 || true
+
+stm32flash-bin/stm32flash_linux -w ./exec.bin -v -g 0x00 /dev/ttyUSB0 || exit 0
 rm ./exec.bin
 rm ./exec
